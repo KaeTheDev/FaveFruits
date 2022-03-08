b0f8feaa27077280c16cab72a232bae1d1dba3b9
ShakiraReidThomas <kaethedev@gmail.com>
2022-03-08 18:55:42 -0500

    Initial Commit

---
+++ FaveFruits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FaveFruits
+//
+//  Created by Shakira Reid-Thomas on 3/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FaveFruits/FaveFruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaveFruitsApp.swift
+//  FaveFruits
+//
+//  Created by Shakira Reid-Thomas on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct FaveFruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
