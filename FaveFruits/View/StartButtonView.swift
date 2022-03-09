9aeaabd49077e8f537a003c26e979af4c0f71c1e
ShakiraReidThomas <kaethedev@gmail.com>
2022-03-08 20:03:44 -0500

    Added all startup files, code, images and colors

---
+++ FaveFruits/View/StartButtonView.swift
@@ -0,0 +1,37 @@
+//
+//  StartButtonView.swift
+//  FaveFruits
+//
+//  Created by Shakira Reid-Thomas on 3/8/22.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    var body: some View {
+        Button(action: {
+            print("Exit the onboarding")
+        }) {
+            HStack(spacing: 8) {
+                Text("Start")
+                
+                Image(systemName: "arrow.right.circle")
+                    .imageScale(.large)
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 10)
+            .background(
+                Capsule().strokeBorder(Color.white, lineWidth: 1.25)
+            )
+        }
+        .accentColor(Color.white)
+    }
+}
+
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+    }
+}
